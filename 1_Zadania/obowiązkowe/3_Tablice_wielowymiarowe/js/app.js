2cf0476f0cb4714cc9b5063909aec30807474bc2
MartinaTybur <49064650+MartinaTybur@users.noreply.github.com>
2019-10-06 10:49:53 +0200

    add fruits

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -1,15 +1,7 @@
 
-var task1Array = [
-    [2, 3],
-    ["Ala", "Ola"],
-    [true, false],
-    [5, 6, 7, 8],
-    [12, 15, 67]
-];
-
-
-var task2Array = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12]
-];
+let fruits = [ 'apple', 'orange', 'banana', 'peach', 'pear'];
+console.log(fruits[0]);
+console.log(fruits[fruits.length - 1]);
+for (let count = 0; count < fruits.length; count ++){
+    console.log(fruits[count]);
+}
