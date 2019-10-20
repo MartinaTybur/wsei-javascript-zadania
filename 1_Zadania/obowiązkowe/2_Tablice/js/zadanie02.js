889024d60b84ee2d09ad53e364481a5edeca6d27
MartinaTybur <49064650+MartinaTybur@users.noreply.github.com>
2019-10-20 11:08:30 +0200

    Update zadanie02.js

--- 1_Zadania/obowiązkowe/2_Tablice/js/zadanie02.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/zadanie02.js
@@ -2,7 +2,7 @@
 function createArray(number) {
     var newArray = [10];
 
-    for (var counter = 1; ; counter <= number) {
+    for (var counter = 1; counter <= number; ) {
         newArray.push(counter);
     }
 
